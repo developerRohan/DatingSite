8effd4e43c6214dffd0ed84e3d96166b6bfe3f12
developerRohan <jhambrohan44@gmail.com>
2018-05-09 15:27:33 +0530

    added home user controller and some external plugins in vendor

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/edit'
+
+  get 'users/profile'
+
+  get 'users/matches'
+
+  get 'home/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
